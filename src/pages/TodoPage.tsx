bd966bae6232a2f94a07866990e1bba77e1dcd55
sayed-kassem <sayed.webdev@gmail.com>
2025-04-13 20:26:27 +0300

    initial

--- src/pages/TodoPage.tsx
+++ src/pages/TodoPage.tsx
@@ -1,10 +1,15 @@
 import { useNavigate } from "react-router-dom";
 import { RootState } from "../app/store"
 import { useDispatch, useSelector } from "react-redux"
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { logout } from "../features/auth/authSlice";
+import { addTodoDB, deleteTodoDB, fetchTodos, toggleTodoDB } from "../features/todos/todoSlice";
 
 export default function TodoPage() {
+
+    const [newTodo, setNewTodo] = useState("")
+    const {todos, loading} = useSelector((state: RootState) => state.todos);
+
 
     const user = useSelector((state: RootState) => state.auth.user)
     const dispatch = useDispatch();
@@ -15,27 +20,66 @@ export default function TodoPage() {
         if(!user) {
             navigate("/login")
         }
-    }, [user, navigate])
+        else{
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            dispatch(fetchTodos() as any)
+        }
+    }, [user, dispatch, navigate])
 
     const handleLogout= () =>{
         dispatch(logout())
         navigate("/login")
     }
 
-    return (
-        <div className="container mt-5">
+    const handleAdd = () => {
+        if(newTodo.trim()){
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            dispatch(addTodoDB(newTodo) as any)
+            setNewTodo("")
+        }
+    }
 
-            <h2>Todo Page</h2>
-            <div className="d-flex justify-content-between align-items-center">
-                <h2>Hello, {user}</h2>
+    const handleToggle = (id: string, completed: boolean) =>{
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        dispatch(toggleTodoDB({id, completed}) as any)
+    }
+
+    const handleDelete = (id: string) => {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        dispatch(deleteTodoDB(id) as any);
+    }
+
+    return (
+        <div className="container mt-4">
+            <div className="d-flex justify-content-between align-items-center mb-4">
+                <h3>Hello, {user}</h3>
                 <button className="btn btn-outline-danger" onClick={handleLogout}>
-                Logout
+                    Logout
                 </button>
             </div>
-            <form action="" method="post">
-                <input type="text" name="todo" id="todo" placeholder="todo" className="form-control mb-2" />
-                <button className="btn btn-primary" type="submit">Add Todo</button>
-            </form>
+
+            <div className="input-group mb-3">
+                <input type="text" className="form-control" value={newTodo}  onChange={(e)=> setNewTodo(e.target.value)} placeholder="New Todo"/>
+                <button className="btn btn-primary" onClick={handleAdd}>Add</button>
+            </div>
+
+            {loading ? (
+                <div className="spinner-border" role="status">
+                    <span className="visually-hidden">Loading...</span>
+                </div>
+            ): (
+                <ul className="list-group">
+                    {todos.map((todo) => (
+                        <li key={todo.id} 
+                            className={`list-group-item d-flex justify-content-between ${todo.completed} ? "text-decoration-line-through" :""`}>
+                             
+                             <span onClick={()=>handleToggle(todo.id, todo.completed)} style={{cursor:"pointer"}}>{todo.text}</span>
+                            <button className="btn btn-sm btn-danger" onClick={()=> handleDelete(todo.id)}>Delete</button>
+                        </li>
+                    ))}
+                </ul>
+            )}
+
         </div>
-    )
+    );
 }
